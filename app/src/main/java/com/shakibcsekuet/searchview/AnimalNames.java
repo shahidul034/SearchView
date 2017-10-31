5711a5cfa2783e559babbc12b664cd600131b1e4
SHAKIB <shakibkhanrock@gmail.com>
2017-10-31 18:53:57 +0600

    first commit

---
+++ app/src/main/java/com/shakibcsekuet/searchview/AnimalNames.java
@@ -0,0 +1,17 @@
+package com.shakibcsekuet.searchview;
+
+/**
+ * Created by md sakib on 9/29/2017.
+ */
+public class AnimalNames {
+    private String animalName;
+
+    public AnimalNames(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName() {
+        return this.animalName;
+    }
+
+}
---
+++ app/src/main/java/com/shakibcsekuet/searchview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.shakibcsekuet.searchview;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    // Declare Variables
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    String[] animalNameList;
+    ArrayList<AnimalNames> arraylist = new ArrayList<AnimalNames>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Generate sample data
+
+        animalNameList = new String[]{"Lion", "Tiger", "Dog",
+                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+                "Cow","Donkey","Monkey"};
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) findViewById(R.id.listview);
+
+        for (int i = 0; i < animalNameList.length; i++) {
+            AnimalNames animalNames = new AnimalNames(animalNameList[i]);
+            // Binds all strings into an array
+            arraylist.add(animalNames);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
+    
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
+    }
+}
